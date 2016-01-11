a297ffe17292a65dd0a6f0fc3a56f17dd69706b9
Erik Dains <eadains@gmail.com>
2016-01-11 15:23:06 -0500

    Profit Factor done. Model needs work.

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -2,16 +2,24 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-def calc_scores(SQL_USER, SQL_PASS, SQL_DATABASE, item_id):
+def calc_scores(SQL_USER, SQL_PASS, SQL_DATABASE, wallet, item_id):
     """
     Calculates profitability scores for specified item_id. Returns Pandas dataframe containing item_id and
-    its score. The profit factor is calculated with (Volume * day low) * Day Profit margin
+    its score. The profit factor is calculated with (Volume * day low) * Day Profit margin. Appends row to given
+    dataframe
     SQL_USER = Database username
     SQL_PASS = Database account pass
     SQL_DATABASE = name of database to use
+    dataframe = Pandas DataFrame object to append to. (For ease of use with multiprocessing)
     """
     engine = create_engine('mysql+mysqlconnector://%s:%s@localhost/%s' % (SQL_USER, SQL_PASS, SQL_DATABASE))
-    frame = pd.read_sql('SELECT FROM prices WHERE item_id=%d' % item_id, engine,
+    frame = pd.read_sql('SELECT * FROM prices WHERE item_id=%d' % item_id, engine,
                         index_col='date', parse_dates=['date'])
-    frame['profit_factor'] = frame['volume'] * frame['low'] * ((frame['high'] - frame['low']) / frame['low'])
-    return pd.DataFrame({'item_id': [frame['item_id'][0]], 'profit_factor': [frame['profit_factor'].mean()]})
+    history_weight = len(frame.index)
+    frame['profit_factor'] = (frame['volume'] * frame['low']) *\
+                             ((frame['high'] - frame['low']) / frame['low']) *\
+                             history_weight * (wallet / frame['low'])
+    try:
+        return pd.DataFrame({'item_id': [frame['item_id'][0]], 'profit_factor': [frame['profit_factor'].mean()]})
+    except IndexError:
+        print 'Item %d has no stored market data. Skipping.' % item_id
--- portfolio_sel.py
+++ portfolio_sel.py
@@ -2,17 +2,20 @@ from portfolio.portfolio import calc_scores
 from sqlalchemy import create_engine
 import pandas as pd
 from functools import partial
-from multiprocessing.pool import Pool
+from multiprocessing import Pool, Queue
 from config.config import SQL_USER, SQL_PASS
 
 
 def main():
     SQL_DATABASE = 'EveMarketData'
+    dataframe = pd.DataFrame()
     engine = create_engine('mysql+mysqlconnector://%s:%s@localhost/%s' % (SQL_USER, SQL_PASS, SQL_DATABASE))
     item_id_list = [int(index) for (index, row) in pd.read_sql_table('items', engine, index_col='item_id').iterrows()]
-    data_write = partial(calc_scores(), SQL_USER, SQL_PASS, SQL_DATABASE)
+    data_write = partial(calc_scores, SQL_USER, SQL_PASS, SQL_DATABASE, 5100000000)
     p = Pool(4)
-    p.map(data_write, item_id_list)
+    result = p.map(data_write, item_id_list)
+    dataframe = dataframe.append(result).sort_values('profit_factor', ascending=False)
+    print dataframe
 
 if __name__ == '__main__':
     main()
